49e995f25a890ccce09d2da0ff9f2372ec8bc88d
jurchiks33 <jurisslisans@gmail.com>
2023-12-22 17:15:00 +0100

    2624

--- JavaScript/2624.Snail Traversal.js
+++ JavaScript/2624.Snail Traversal.js
@@ -83,3 +83,9 @@ Array.prototype.snail = function(rowsCount, colsCount) {
 
     return result;
 };
+
+// Example usage
+const arr = [19, 10, 3, 7, 9, 8, 5, 2, 1, 17, 16, 14, 12, 18, 6, 13, 11, 20, 4, 15];
+console.log(arr.snail(5, 4));
+
+//Accepted
