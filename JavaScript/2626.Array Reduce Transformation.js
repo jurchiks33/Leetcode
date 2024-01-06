e48e4f0d3a1b0da81ecdbe55829fa034f517d253
jurchiks33 <jurisslisans@gmail.com>
2024-01-06 19:51:18 +0100

    2626

--- JavaScript/2626.Array Reduce Transformation.js
+++ JavaScript/2626.Array Reduce Transformation.js
@@ -11,3 +11,33 @@
 // If the length of the array is 0, the function should return init.
 
 // Please solve it without using the built-in Array.reduce method.
+
+// Example 1:
+
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr; }
+// init = 0
+// Output: 10
+// Explanation:
+// initially, the value is init=0.
+// (0) + nums[0] = 1
+// (1) + nums[1] = 3
+// (3) + nums[2] = 6
+// (6) + nums[3] = 10
+// The final answer is 10.
+
+// Example 2:
+
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr * curr; }
+// init = 100
+// Output: 130
+// Explanation:
+// initially, the value is init=100.
+// (100) + nums[0] * nums[0] = 101
+// (101) + nums[1] * nums[1] = 105
+// (105) + nums[2] * nums[2] = 114
+// (114) + nums[3] * nums[3] = 130
+// The final answer is 130.
